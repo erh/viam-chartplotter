d0b56c590dbd37e275669b7da1fb6f579c9fd2a2
Eliot Horowitz <eliothorowitz@gmail.com>
2025-03-05 22:58:25 -0500

    fix github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+    kit: {
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/viam-chartplotter' : '',
+        }
+        
+    }
 };
 
 export default config;
